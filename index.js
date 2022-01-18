b7c870bc55872344171b7536598d2ee0556e11cb
micrus <michele.russo@studenti.unicam.it>
2022-01-18 17:44:03 +0100

    Minor changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { GraphVisualizer, Diagram, Tangle, TangleVisualizer } from "./src/main.js";
 
 let diagram = new Diagram("2:7,3:5,4:6");
-let graphVisualizer = new GraphVisualizer(diagram.toTangle().toDiagram(), "body", 500, 300);
-let tangleVisualizer = new TangleVisualizer(diagram.toTangle(), "body", 500,300);
+let graphVisualizer = new GraphVisualizer(diagram, "body", 500, 300, {dot_color:"red",arc_line:"zigzed"});
+//let tangleVisualizer = new TangleVisualizer(diagram.toTangle(), "body", 500,300);
 
 graphVisualizer.draw()
-tangleVisualizer.draw()
+//tangleVisualizer.draw()
 
 
 /*
