25d6fa2974b3432fbac6a15a9d3b5aeced408b44
micrus <michele.russo@studenti.unicam.it>
2022-01-25 18:53:31 +0100

    Minor changes in example

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { GraphVisualizer, Diagram, Tangle, TangleVisualizer } from "./src/main.js";
 
-let diagram = new Diagram("2:7,3:5,4:6");
-let graphVisualizer = new GraphVisualizer(diagram, "body", 500, 300, {dot_color:"red",arc_line:"zigzed"});
+let diagram = new Tangle("1':1,2':15,3':17,4':10',5':8',6':7',9':14',11':12',13':17',15':16',2:16,3:14,4:13,5:12,6:11,7:10,8:9");
+let graphVisualizer = new TangleVisualizer(diagram, "body", 800, 900);
 //let tangleVisualizer = new TangleVisualizer(diagram.toTangle(), "body", 500,300);
 
 graphVisualizer.draw()
